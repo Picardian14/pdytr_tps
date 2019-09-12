760f027f0fb65d4fdb01c49fd45ffb3df059a0b4
picardian14 <mindlinster@gmail.com>
2019-09-11 23:25:08 -0300

    o

---
+++ TP1_WillIvan/client.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <string.h>
+#include <time.h>
+
+#define BUF_SIZE 4080
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[BUF_SIZE+16];
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+	//TOMA EL NUMERO DE PUERTO DE LOS ARGUMENTOS
+    portno = atoi(argv[2]);
+	
+	//CREA EL FILE DESCRIPTOR DEL SOCKET PARA LA CONEXION
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	//AF_INET - FAMILIA DEL PROTOCOLO - IPV4 PROTOCOLS INTERNET
+	//SOCK_STREAM - TIPO DE SOCKET 
+	
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+	
+	//TOMA LA DIRECCION DEL SERVER DE LOS ARGUMENTOS
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+	
+	//COPIA LA DIRECCION IP Y EL PUERTO DEL SERVIDOR A LA ESTRUCTURA DEL SOCKET
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+     serv_addr.sin_port = htons(portno);
+	
+	//DESCRIPTOR - DIRECCION - TAMAÑO DIRECCION
+    if (connect(sockfd,&serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+    printf("Please enter the message: ");
+    
+    bzero(buffer, BUF_SIZE);
+    fgets(buffer,BUF_SIZE,stdin);
+
+    //CHECKsuma
+    int amount_read =strlen(buffer);
+    
+    int num_packets = (amount_read / 16);
+    int padding = amount_read % 16;
+    char suma[16], checksuma[16];
+    bzero(suma, 16);
+    
+    for (int i=0; i<num_packets;i++){
+        for(int j=0; j<16;j++){
+            suma[j]=suma[j]+buffer[(i*16)+j];
+        }
+    }
+    for(int k=0;k<padding;k++){
+        suma[k]=suma[k]+buffer[(num_packets*16)+k];
+    }
+    
+    for(int i=0; i<16;i++){
+        checksuma[i]=~suma[i];
+    }
+    
+    
+    //FIN CHECKsuma
+    //ENVIA UN MENSAJE AL SOCKET
+    
+    
+    //strcat(buffer,checksuma);
+    //printf("%s\n", buffer);
+    for(int i=0;i<16;i++){
+        buffer[amount_read+i]=checksuma[i];
+    }
+    printf("%d", strlen(checksuma));
+    //printf("%s", buffer);
+
+    clock_t start, end=0;
+    double cpu_time;
+    start = clock();
+    FILE *fp;
+    fp = fopen("/home/mrmister/Facultad/4to/Pdytr/tiempos","a+");
+    if(fp ==NULL) printf("no se abrio");
+    
+	n = write(sockfd,buffer,strlen(buffer));
+    end = clock();
+    if (n < 0) 
+         error("ERROR writing to socket");
+    bzero(buffer,BUF_SIZE);
+	
+    //ESPERA RECIBIR UNA RESPUESTA
+	n = read(sockfd,buffer,BUF_SIZE);
+    end = clock();
+    cpu_time = end - start;
+    fprintf(fp, "%f\n", cpu_time);
+    close(fp);
+    if (n < 0) 
+         error("ERROR reading from socket");
+    
+    return 0;
+}
+
---
+++ TP1_WillIvan/server.c
@@ -0,0 +1,99 @@
+/* A simple server in the internet domain using TCP
+   The port number is passed as an argument */
+#include <stdio.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#define BUF_SIZE 4080
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+     int sockfd, newsockfd, portno, clilen;
+     char buffer[BUF_SIZE];
+     struct sockaddr_in serv_addr, cli_addr;
+     int n;
+     if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+     }
+	 //CREA EL FILE DESCRIPTOR DEL SOCKET PARA LA CONEXION
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	 //AF_INET - FAMILIA DEL PROTOCOLO - IPV4 PROTOCOLS INTERNET
+	//SOCK_STREAM - TIPO DE SOCKET 
+	
+     if (sockfd < 0) 
+        error("ERROR opening socket");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     //ASIGNA EL PUERTO PASADO POR ARGUMENTO
+	 //ASIGNA LA IP EN DONDE ESCUCHA (SU PROPIA IP)
+	 portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+	 
+	 //VINCULA EL FILE DESCRIPTOR CON LA DIRECCION Y EL PUERTO
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0) 
+              error("ERROR on binding");
+			  
+	 //SETEA LA CANTIDAD QUE PUEDEN ESPERAR MIENTRAS SE MANEJA UNA CONEXION		  
+     listen(sockfd,5);
+	 
+	 // SE BLOQUEA A ESPERAR UNA CONEXION
+     clilen = sizeof(cli_addr);
+     newsockfd = accept(sockfd, 
+                 (struct sockaddr *) &cli_addr, 
+                 &clilen);
+				 
+     //DEVUELVE UN NUEVO DESCRIPTOR POR EL CUAL SE VAN A REALIZAR LAS COMUNICACIONES
+	 if (newsockfd < 0) 
+          error("ERROR on accept");
+     bzero(buffer,BUF_SIZE);
+
+	//LEE EL MENSAJE DEL CLIENTE
+     n = read(newsockfd,buffer,BUF_SIZE+16);
+
+     //HACEMOS CHECKSUM
+     int size_sent = strlen(buffer) - 16;
+     int num_packets = (size_sent)/16;
+     int padding = size_sent % 16;
+     char sum[16], checksum[16];
+     bzero(sum,16);
+     
+     for (int i=0; i<num_packets;i++){
+        for(int j=0; j<16;j++){
+            sum[j]=sum[j]+buffer[(i*16)+j];
+        }
+    }
+    for(int k=0;k<padding;k++){
+        sum[k]=sum[k]+buffer[num_packets*16+k];
+    }
+    for(int i=0; i<16;i++){
+         checksum[i] = buffer[size_sent+i];
+    }
+    char prueba[16];
+     for(int k=0; k<16;k++){
+          if (~(checksum[k] ^ sum[k])){
+               error("Error en el checksum. Paquete corrompido");
+          }  
+     }
+     char rcvd_message[size_sent];
+     printf("El paquete llego sin errores");
+     for (int i=0;i<size_sent;i++){
+          rcvd_message[i]=buffer[i];
+     }
+     printf("\n");
+     if (n < 0) error("ERROR reading from socket");
+     //printf("%d", strlen(checksum));
+     printf("Here is the message: %s\n",rcvd_message);
+	 
+	 //RESPONDE AL CLIENTE
+     n = write(newsockfd,"I got your message",18);
+     if (n < 0) error("ERROR writing to socket");
+     return 0; 
+}
